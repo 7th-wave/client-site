31cb81c233aa4f68ba76b2da08897c0c9dd9fabe
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2022-02-28 11:18:22 +0100

    change to slot props

--- src/components/cards/CategoryCard.vue
+++ src/components/cards/CategoryCard.vue
@@ -1,11 +1,11 @@
 <template>
     <div class=" w-full bg-white rounded-md overflow-hidden shadow-lg">
         <div class=" w-full h-72 cursor-pointer">
-            <img class=" w-full h-full object-cover" :src="category.image" alt="">
+            <slot name="image"/>
         </div>
         <div class=" flex flex-col items-start space-y-2 w-full bg-white py-4 px-6">
-            <span class=" text-sm font-inter font-medium text-teal-500 cursor-pointer">{{category.items}} items</span>
-            <span class=" text-gray-900 text-xl font-semibold font-inter">{{category.name}}</span>
+            <slot  name="subtitle"/>
+            <slot name="title"/>
         </div>
     </div>
 </template>
@@ -14,11 +14,6 @@
 
 <script>
 export default {
-    props: {
-        category: {
-            type: Object,
-            required: true,
-        },
-    },
+   
 }
 </script>
--- src/views/frontend/Explore.vue
+++ src/views/frontend/Explore.vue
@@ -13,7 +13,32 @@
           v-for="(category, index, key) in categories"
           :key="key"
           :category="category"
-        />
+        >
+          <template #image>
+            <img
+              class="w-full h-full object-cover"
+              :src="category.image"
+              alt=""
+            />
+          </template>
+
+          <template #subtitle>
+            <span
+              class="
+                text-sm
+                font-inter font-medium
+                text-teal-500
+                cursor-pointer
+              "
+              >{{ category.items }} items</span
+            >
+          </template>
+          <template #title>
+            <span class="text-gray-900 text-xl font-semibold font-inter">{{
+              category.name
+            }}</span>
+          </template>
+        </CategoryCard>
       </div>
     </template>
   </InteriorPage>
@@ -21,68 +46,78 @@
 
 
 <script>
-import InteriorPage from '../../components/Layouts/InteriorPage.vue';
-import CategoryCard from '../../components/cards/CategoryCard.vue';
+import InteriorPage from "../../components/Layouts/InteriorPage.vue";
+import CategoryCard from "../../components/cards/CategoryCard.vue";
 export default {
-    components:{
-        InteriorPage,
-        CategoryCard
-    },
-    data(){
-        return {
-            categories: [
-            {
-                name: 'Sneakers',
-                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
-                items: 2
-            },
-            {
-                name: 'Boots',
-                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
-                items: 2
-            },
-            {
-                name: 'Sandals',
-                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
-                items: 2
-            },
-            {
-                name: 'Flip Flops',
-                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
-                items: 2
-            },
-            {
-                name: 'Sneakers',
-                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
-                items: 2
-            },
-            {
-                name: 'Boots',
-                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
-                items: 2
-            },
-            {
-                name: 'Sandals',
-                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
-                items: 2
-            },
-            {
-                name: 'Flip Flops',
-                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
-                items: 2
-            },
-            {
-                name: 'Sneakers',
-                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
-                items: 2
-            },
-            {
-                name: 'Boots',
-                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
-                items: 2
-            },
-        ]
-        }
-    }
-}
+  components: {
+    InteriorPage,
+    CategoryCard,
+  },
+  data() {
+    return {
+      categories: [
+        {
+          name: "Sneakers",
+          image:
+            "https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg",
+          items: 2,
+        },
+        {
+          name: "Boots",
+          image:
+            "https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg",
+          items: 2,
+        },
+        {
+          name: "Sandals",
+          image:
+            "https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg",
+          items: 2,
+        },
+        {
+          name: "Flip Flops",
+          image:
+            "https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg",
+          items: 2,
+        },
+        {
+          name: "Sneakers",
+          image:
+            "https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg",
+          items: 2,
+        },
+        {
+          name: "Boots",
+          image:
+            "https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg",
+          items: 2,
+        },
+        {
+          name: "Sandals",
+          image:
+            "https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg",
+          items: 2,
+        },
+        {
+          name: "Flip Flops",
+          image:
+            "https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg",
+          items: 2,
+        },
+        {
+          name: "Sneakers",
+          image:
+            "https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg",
+          items: 2,
+        },
+        {
+          name: "Boots",
+          image:
+            "https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg",
+          items: 2,
+        },
+      ],
+    };
+  },
+};
 </script>
