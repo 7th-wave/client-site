08b428206d3c22c72e00872d3d32f675557a732f
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2022-02-28 09:33:02 +0100

    Vault Categories

---
+++ src/components/cards/CategoryCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class=" w-full bg-white rounded-md overflow-hidden shadow-lg">
+        <div class=" w-full h-72 cursor-pointer">
+            <img class=" w-full h-full object-cover" :src="category.image" alt="">
+        </div>
+        <div class=" flex flex-col items-start space-y-2 w-full bg-white py-4 px-6">
+            <span class=" text-sm font-inter font-medium text-teal-500 cursor-pointer">{{category.items}} items</span>
+            <span class=" text-gray-900 text-xl font-semibold font-inter">{{category.name}}</span>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    props: {
+        category: {
+            type: Object,
+            required: true,
+        },
+    },
+}
+</script>
--- src/views/frontend/Explore.vue
+++ src/views/frontend/Explore.vue
@@ -1,5 +1,88 @@
 <template>
-    <div>
-        <h1>Explore Page</h1>
-    </div>
+  <InteriorPage>
+    <template #body>
+      <div class="flex pb-10">
+        <span
+          class="m-auto text-4xl text-center font-normal font-inter text-black"
+          >Vault Categories</span
+        >
+      </div>
+
+      <div class="grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 gap-10">
+        <CategoryCard
+          v-for="(category, index, key) in categories"
+          :key="key"
+          :category="category"
+        />
+      </div>
+    </template>
+  </InteriorPage>
 </template>
+
+
+<script>
+import InteriorPage from '../../components/Layouts/InteriorPage.vue';
+import CategoryCard from '../../components/cards/CategoryCard.vue';
+export default {
+    components:{
+        InteriorPage,
+        CategoryCard
+    },
+    data(){
+        return {
+            categories: [
+            {
+                name: 'Sneakers',
+                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
+                items: 2
+            },
+            {
+                name: 'Boots',
+                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
+                items: 2
+            },
+            {
+                name: 'Sandals',
+                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
+                items: 2
+            },
+            {
+                name: 'Flip Flops',
+                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
+                items: 2
+            },
+            {
+                name: 'Sneakers',
+                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
+                items: 2
+            },
+            {
+                name: 'Boots',
+                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
+                items: 2
+            },
+            {
+                name: 'Sandals',
+                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
+                items: 2
+            },
+            {
+                name: 'Flip Flops',
+                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
+                items: 2
+            },
+            {
+                name: 'Sneakers',
+                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
+                items: 2
+            },
+            {
+                name: 'Boots',
+                image: 'https://cdn.corporatefinanceinstitute.com/assets/types-of-assets-1024x575.jpeg',
+                items: 2
+            },
+        ]
+        }
+    }
+}
+</script>
