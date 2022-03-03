fe8fce89b269bae3f5ab1d35db7d281178edb144
Rogers Sampaio <rogers.sampaio@vitaminaweb.com.br>
2022-03-03 11:24:27 -0300

    Change colors

--- src/App.vue
+++ src/App.vue
@@ -23,9 +23,8 @@ import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
 import Modal from './components/Modals/Modal.vue';
 import Loading from './components/Shared/Loading.vue';
-import WalletConnectClient from "@walletconnect/client";
+import WalletConnect from "@walletconnect/client";
 
-import { CLIENT_EVENTS } from "@walletconnect/client";
 import QRCodeModal from "@walletconnect/qrcode-modal";
 
 
@@ -60,29 +59,16 @@ export default {
 
       }
 
-      const client = await WalletConnectClient.init({
-        projectId: "c77630296e835e9f15911e4957965e64",
-        relayUrl: "wss://relay.walletconnect.com",
-        metadata: {
-          name: "Jupiter Exchange",
-          description: "Jupiter Exchange",
-          url: "#",
-          icons: ["https://walletconnect.com/walletconnect-logo.png"],
-        },
+      const connector = new WalletConnect({
+        bridge: "https://bridge.walletconnect.org", // Required
+        qrcodeModal: QRCodeModal,
       });
 
-      client.on(
-        CLIENT_EVENTS.pairing.proposal,
-        async (proposal) => {
-          // uri should be shared with the Wallet either through QR Code scanning or mobile deep linking
-          const { uri } = proposal.signal.params;
-          console.log(uri);
-          QRCodeModal.open(uri, () => {
-            console.log("EVENT", "QR Code Modal closed");
-          });
-          
-        }
-      );
+      // Check if connection is already established
+      if (!connector.connected) {
+        // create new session
+        connector.createSession();
+      }
 
       
     })
