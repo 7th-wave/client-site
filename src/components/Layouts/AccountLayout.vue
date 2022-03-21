8d474bbc6476cf5f1b89378fb05a84598ded2776
Rogers Sampaio <rogers.sampaio@vitaminaweb.com.br>
2022-03-21 15:27:23 -0300

    layout changes

--- src/components/Layouts/AccountLayout.vue
+++ src/components/Layouts/AccountLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="account sm:px-0 pr-4 py-4 sm:py-4 lg:py-16 relative mx-auto bg-gray-100 font-inter max-w-7xl w-full"
+    class="account sm:px-8 pr-4 py-4 sm:py-4 lg:py-16 relative mx-auto bg-gray-100 font-inter max-w-7xl w-full"
   >
     <slot></slot>
   </div>
--- src/views/client/NftCreate.vue
+++ src/views/client/NftCreate.vue
@@ -1,13 +1,13 @@
 <template>
   <account-layout>
-    <div class="lg:hidden">
+    <div class="xl:hidden">
       <Navbar />
     </div>
     <div class="py-0 sm:py-4 md:pb-8 md:pt-4 lg:pt-0 lg:pb-8 text-center">
       <h1 class="sm:text-4xl text-2xl font-medium">NFT Mint New Asset(s)</h1>
     </div>
     <div class="space-y-2">
-      <div class=" sm:grid sm:grid-cols-7 gap-8">
+      <div class=" xl:grid xl:grid-cols-7 xl:gap-8 sm:m-auto">
         <div class="sm:col-span-2 hidden xl:block">
           <Menu />
         </div>
