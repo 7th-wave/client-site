50cedacb7744a5463bfa76d9f688c5669abf34f7
hamigg <h@7thw.com>
2022-06-18 14:43:44 -0400

    Under Construction

--- src/router/index.js
+++ src/router/index.js
@@ -119,6 +119,11 @@ const routes = [
     component: () => import("../views/frontend/LearnMore.vue"),
   },
   {
+    path: "/Under-Construction",
+    name: "UnderConstruction",
+    component: () => import("../views/frontend/UnderConstruction.vue"),
+  },
+  {
     path: "/vault/:id",
     name: "Vault",
     component: () => import("../views/frontend/Vault.vue"),
