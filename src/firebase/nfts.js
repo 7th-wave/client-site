20800c7e2a67feb562f94d6ab79fa01e43939861
Rogers Sampaio <rogerssampaio@Rogerss-MacBook-Pro.local>
2022-08-13 20:10:09 -0300

    home

--- src/firebase/nfts.js
+++ src/firebase/nfts.js
@@ -3,6 +3,16 @@ import axios from 'axios';
 
 const app_url = process.env.VUE_APP_FUNCTIONS_URL;
 
+const getMintedNftsCount = async() => {
+    //const result = await axios.get(app_url+'find_minted_nfts');
+    return 1;
+}
+
+const filterNfts = async(data) => {
+    const result = await axios.post(app_url+'filter_nfts', data);
+    return result.data;
+}
+
 const getNfts = async(page) => {
     const result = await axios.get(app_url+'find_nfts?page='+page);
     return result.data;
@@ -38,6 +48,105 @@ const updateNft = async(ref, data) => {
     return result.data;
 }
 
+const nftFilters = [
+    {
+        name: 'Bear Bod',
+        key: 'bear_bod',
+        options: [
+            {
+                name: 'Time',
+                value: 'Time'
+            },
+            {
+                name: 'OG',
+                value: 'OG'
+            }
+        ]
+    },
+    {
+        name: 'Bear Flavr',
+        key: 'bear_flavr',
+        options: [
+            {
+                name: 'BlueBeary',
+                value: 'BlueBeary'
+            },
+            {
+                name: 'Moon',
+                value: 'Moon'
+            }
+        ]
+    },
+    {
+        name: 'Shades',
+        key: 'shades',
+        options: [
+            {
+                name: 'PixOrange',
+                value: 'PixOrange'
+            },
+            {
+                name: 'PixBlue',
+                value: 'PixBlue'
+            }
+        ]
+    },
+    {
+        name: 'Canvas',
+        key: 'canvas',
+        options: [
+            {
+                name: 'Lil Havana',
+                value: 'Lil Havana'
+            },
+            {
+                name: 'Wynwood',
+                value: 'Wynwood'
+            }
+        ]
+    },
+    {
+        name: 'Cirkol Class',
+        key: 'cirkol_class',
+        options: [
+            {
+                name: 'Lil Havana',
+                value: 'Lil Havana'
+            },
+            {
+                name: 'Wynwood',
+                value: 'Wynwood'
+            }
+        ]
+    },
+    {
+        name: 'Member Stars',
+        key: 'member_stars',
+        options: [
+            {
+                name: '1',
+                value: 1
+            },
+            {
+                name: '2',
+                value: 2
+            },
+            {
+                name: '3',
+                value: 3
+            },
+            {
+                name: '4',
+                value: 4
+            },
+            {
+                name: '5',
+                value: 5
+            }
+        ]
+    }
+]
+
 
 
 export {
@@ -47,5 +156,8 @@ export {
     updateNft,
     getNfts,
     getNftsByCollection,
-    findNewxtIdPerContract
+    findNewxtIdPerContract,
+    getMintedNftsCount,
+    nftFilters,
+    filterNfts
 };
--- src/views/frontend/Home.vue
+++ src/views/frontend/Home.vue
@@ -1,55 +1,137 @@
 <template>
   <div class="artist font-inter h-screen">
-    <div class="relative bg-white">
-      <div class="relative">
-        <div class="text-center mx-auto w-full mb-4 ">
-          
-          <h1 class="mt-8 text-4xl leading-10 flex items-center justify-center font-bold text-white tracking-tight sm:text-4xl header-height" style="background:url('./images/CIRKOL_Header-market.jpg') rgba(0, 0, 0, 0.5); background-blend-mode: multiply; background-size: cover">
+    <div class="relative bg-white shadow-lg pb-8">
+      <div class="relative z-20">
+        <div class="text-center mx-auto w-full mb-4">
+          <h1
+            class="
+              mt-8
+              text-4xl
+              leading-10
+              flex
+              items-center
+              justify-center
+              font-bold
+              text-white
+              tracking-tight
+              sm:text-4xl
+              header-height
+            "
+            style="
+              background: url('./images/CIRKOL_Header-market.jpg')
+                rgba(0, 0, 0, 0.5);
+              background-blend-mode: multiply;
+              background-size: cover;
+            "
+          >
             MNFT MIAMI
-          </h1>  
+          </h1>
 
-          <p class="mt-6 max-w-md px-4 sm:max-w-3xl lg:max-w-6xl mx-auto text-center text-lg text-primary-700 font-bold">CIRKOL is the first membership club of its kind.</p>           
+          <p
+            class="
+              mt-6
+              max-w-md
+              px-4
+              sm:max-w-3xl
+              lg:max-w-6xl
+              mx-auto
+              text-center text-lg text-primary-700
+              font-bold
+            "
+          >
+            CIRKOL is the first membership club of its kind.
+          </p>
 
-          <p class="mt-6 max-w-md px-4 sm:max-w-3xl lg:max-w-6xl mx-auto text-justify text-primary-700">CIRKOL blends decentralized technology with our innate desire to connect with people in the physical world.<br /> Our experienced team has prepared a proprietary pro forma for the purposes of acquisition and redevelopment of a hotel and beach club in Miami, Florida. Once the project treasury goal is achieved, we will begin to act on these development goals and ultimately offer prime access to utilize incredible on - property amenities to our mNFT(membership Non Fungible Token) holders.mNFT holders will also have the opportunity to buy and resell rNFTs(reservation Non Fungible Tokens) that operate as a decentralized booking platform for the hotel and beach club.<br /> Our first collection features artwork from Miami artist, Manfred Delgado, and is limited to an exclusive 5500 mNFTs minted using our proprietary marketplace.</p>           
+          <p
+            class="
+              mt-6
+              max-w-md
+              px-4
+              sm:max-w-3xl
+              lg:max-w-6xl
+              mx-auto
+              text-justify text-primary-700
+            "
+          >
+            CIRKOL blends decentralized technology with our innate desire to
+            connect with people in the physical world.<br />
+            Our experienced team has prepared a proprietary pro forma for the
+            purposes of acquisition and redevelopment of a hotel and beach club
+            in Miami, Florida. Once the project treasury goal is achieved, we
+            will begin to act on these development goals and ultimately offer
+            prime access to utilize incredible on - property amenities to our
+            mNFT(membership Non Fungible Token) holders.mNFT holders will also
+            have the opportunity to buy and resell rNFTs(reservation Non
+            Fungible Tokens) that operate as a decentralized booking platform
+            for the hotel and beach club.<br />
+            Our first collection features artwork from Miami artist, Manfred
+            Delgado, and is limited to an exclusive 5500 mNFTs minted using our
+            proprietary marketplace.
+          </p>
         </div>
-        
+      </div>
+      <div class="max-w-7xl mx-auto">
+        <Stats :stats="stats" />
+      </div>
+      <div class="max-w-7xl mx-auto">
+        <Filters :filters="localFilters" @on:filter="showNfts" />
       </div>
     </div>
+
     <div class="pb-20 pt-6 white bg-white">
-      <div v-if="isLoading" class="box-border max-w-7xl grid grid-cols-1 lg:grid-cols-4 gap-4 mx-auto before:box-inherit after:box-inherit">
-          <section
-            v-for="index in 12"
-            :key="index"
-            class="relative rounded-lg mx-3 mb-8 md:mx-0 md:mb-5 md:mt-0"
+      <div
+        v-if="isLoading"
+        class="
+          box-border
+          max-w-7xl
+          grid grid-cols-1
+          lg:grid-cols-4
+          gap-4
+          mx-auto
+          before:box-inherit
+          after:box-inherit
+        "
+      >
+        <section
+          v-for="index in 12"
+          :key="index"
+          class="relative rounded-lg mx-3 mb-8 md:mx-0 md:mb-5 md:mt-0"
+        >
+          <div class="content-center">
+            <nft-skeleton />
+          </div>
+        </section>
+      </div>
+      <div
+        class="
+          box-border
+          max-w-7xl
+          grid grid-cols-1
+          lg:grid-cols-4
+          gap-4
+          mx-auto
+          before:box-inherit
+          after:box-inherit
+        "
+        ref="scrollComponent"
+        v-else
+      >
+        <section
+          v-for="art in data.gallery"
+          :key="art.id"
+          class="relative rounded-lg mx-3 mb-8 md:mx-0 md:mb-5 md:mt-0"
+        >
+          <div
+            class="content-center"
+            :class="{ 'flex flex-nowrap h-48': art.id == 1 }"
           >
-            <div
-              class="content-center"
-            >
-              <nft-skeleton />
-              
-            </div>
-          </section>
-        </div>
-       <div class="box-border max-w-7xl grid grid-cols-1 lg:grid-cols-4 gap-4 mx-auto before:box-inherit after:box-inherit" ref="scrollComponent" v-else>
-          <section
-            v-for="art in data.gallery"
-            :key="art.id"
-            class="relative rounded-lg mx-3 mb-8 md:mx-0 md:mb-5 md:mt-0"
-          >
-            <div
-              class="content-center"
-              :class="{ 'flex flex-nowrap h-48': art.id == 1 }"
-            >
-              <router-link :to="artLink(art)" class="cursor-pointer">
-              <nft-item :nft="art" :price="price" :amount="amount"  />
-              </router-link>
-              
-            </div>
-          </section>
-        </div>
-        
+            <router-link :to="artLink(art)" class="cursor-pointer">
+              <nft-item :nft="art" :price="price" :amount="amount" />
+            </router-link>
+          </div>
+        </section>
+      </div>
     </div>
-    
   </div>
 </template>
 
@@ -57,20 +139,23 @@
 <script>
 // @ is an alias to /src
 //import Gallery from '@/components/Gallery/Gallery';
-import NftItem from '@/components/Shared/NftItem.vue';
+import NftItem from "@/components/Shared/NftItem.vue";
 //import { useRoute } from 'vue-router';
-import { computed, ref } from '@vue/reactivity';
-import { onMounted, onUnmounted } from '@vue/runtime-core';
-import { getNftsByCollection } from '../../firebase/nfts';
+import { computed, ref } from "@vue/reactivity";
+import { onMounted, onUnmounted } from "@vue/runtime-core";
+import { getNftsByCollection, nftFilters, getMintedNftsCount, filterNfts } from "../../firebase/nfts";
+import { getMembersCount } from "../../firebase/clients";
 import { storage } from "../../firebase/firebase";
-import NftSkeleton from '@/components/Shared/NftSkeleton.vue';
-import { getWhiteList } from '../../blockchain/index';
-import { useStore } from 'vuex';
-import { useRoute } from 'vue-router';
+import NftSkeleton from "@/components/Shared/NftSkeleton.vue";
+import { getWhiteList } from "../../blockchain/index";
+import { useStore } from "vuex";
+import { useRoute } from "vue-router";
+import Stats from "@/components/Shared/Stats.vue";
+import Filters from "@/components/Shared/Filters.vue";
 
 //import { useStore } from 'vuex';
 
- /* const data = {
+/* const data = {
     artist: 'Mario Sanchez',
     title: 'Mario Sanchez Collection',
     artist_name: 'Mario Sanchez',
@@ -80,76 +165,100 @@ import { useRoute } from 'vue-router';
 
  } */
 
- 
 export default {
   components: {
     //Gallery
-   NftItem,
-    NftSkeleton
-
+    NftItem,
+    NftSkeleton,
+    Stats,
+    Filters,
   },
-  emits: ['on:connect'],
-  setup(props, {emit}) {
+  emit: ["on:connect"],
+  setup(props, { emit }) {
     const route = useRoute();
     const connect = computed(() => route.params.connect);
 
-
     const store = useStore();
     //const collectionRef = route.params.ref;
-    const addr = computed(() => store.getters['blockchain/getCurrentAddress']);
-    const instance = computed(() => store.getters['blockchain/getInstance']);
+    const addr = computed(() => store.getters["blockchain/getCurrentAddress"]);
+    const instance = computed(() => store.getters["blockchain/getInstance"]);
 
     const price = ref(0);
     const amount = ref(0);
+    const localFilters = ref(nftFilters);
+    const stats = ref([
+      {
+        name: "mNft",
+        value: 5500,
+      },
+      {
+        name: "Minted",
+        value: 0,
+      },
+      {
+        name: "Members",
+        value: 0,
+      },
+    ]);
 
     //const storage = firebase.storage();
     const currentPage = ref(1);
     const scrollComponent = ref();
     const data = ref({
-      title: 'Collection Name',
-      artist: 'Manfred',
-      artist_name: 'Manfred',
-      bio_title: 'This is the bio',
-      bio: 'This is the bio',
-      img: './images/mocks/nfts/img_01.jpg',
-      gallery: []
+      title: "Collection Name",
+      artist: "Manfred",
+      artist_name: "Manfred",
+      bio_title: "This is the bio",
+      bio: "This is the bio",
+      img: "./images/mocks/nfts/img_01.jpg",
+      gallery: [],
     });
     const isLoading = ref(true);
 
-    const getData = async () => {    
+    const getData = async () => {
+      const gallery = await getNftsByCollection(
+        process.env.VUE_APP_CATEGORY,
+        currentPage.value
+      );
       
-      const gallery = await getNftsByCollection(process.env.VUE_APP_CATEGORY, currentPage.value);
 
-      data.value.gallery = await Promise.all(gallery.map(async (item) => {
-        
+      data.value.gallery = await Promise.all(
+        gallery.map(async (item) => {
           const data = {
             id: item.dbRef,
             title: item.title,
             href: "/admin/artwork/" + item.collection + "/" + item.dbRef,
             size: item.size,
-            category: 'mnft-miami',
+            category: "mnft-miami",
             collection: item.collection,
             imageUrl: await getFullImageURL(item.imageUrl),
             isMinted: item.isMinted,
-            mintinPrice: item.mintinPrice
+            mintinPrice: item.mintinPrice,
           };
 
-
-         return data;
-        
-      }))
+          return data;
+        })
+      );
 
       // doc.data() is never undefined for query doc snapshots
-      data.value.artist_name = 'MNFT-MIAMI';
-      data.value.title =  'MNFT-MIAMI';
-      data.value.bio_title = 'MNFT-MIAMI';
-      data.value.lastName = 'MNFT-MIAMI';
-      data.value.bio = 'MNFT-MIAMI';
+      data.value.artist_name = "MNFT-MIAMI";
+      data.value.title = "MNFT-MIAMI";
+      data.value.bio_title = "MNFT-MIAMI";
+      data.value.lastName = "MNFT-MIAMI";
+      data.value.bio = "MNFT-MIAMI";
 
       isLoading.value = false;
 
-        
-    }
+      const minted = await getMintedNftsCount();
+      const memberCount = await getMembersCount();
+
+      const sts = stats.value;
+      console.log(sts);
+      sts[1].value = minted;
+      sts[2].value = memberCount;
+
+      stats.value = sts;
+    };
 
     const getFullImageURL = async (item) => {
       if (item) {
@@ -162,73 +271,104 @@ export default {
 
     const getMore = async () => {
       currentPage.value++;
-      const gallery = await getNftsByCollection(process.env.VUE_APP_CATEGORY, currentPage.value);
-      const newItems = await Promise.all(gallery.map(async (item) => {
-        
+      const gallery = await getNftsByCollection(
+        process.env.VUE_APP_CATEGORY,
+        currentPage.value
+      );
+      const newItems = await Promise.all(
+        gallery.map(async (item) => {
           const data = {
             id: item.dbRef,
             title: item.title,
             href: "/admin/artwork/" + item.collection + "/" + item.dbRef,
             size: item.size,
-            category: 'mnft-miami',
+            category: "mnft-miami",
             mintinPrice: item.mintinPrice,
             collection: item.collection,
             imageUrl: await getFullImageURL(item.imageUrl),
           };
 
-         return data;
-        
-      }))
-      data.value.gallery.push(...newItems)
-    }
+          return data;
+        })
+      );
+      data.value.gallery.push(...newItems);
+    };
 
-    const handleScroll = async() => {
-      let element = scrollComponent.value
+    const handleScroll = async () => {
+      let element = scrollComponent.value;
       if (element.getBoundingClientRect().bottom < window.innerHeight) {
         await getMore();
       }
-    } 
+    };
 
     const artLink = (artwork) => {
       return "/artwork/" + artwork.collection + "/" + artwork.id;
     };
 
-    onMounted(async() => {
-      if (connect.value == 'yes') {
-        emit('on:login');
-       }
-      await store.dispatch('blockchain/initWallets');
-      await store.dispatch('blockchain/getBlockChain');
+    const showNfts = async (event) => {
+      isLoading.value = true;
+
+      const gallery = await filterNfts({filter: event.filtersSelected});
+
+      data.value.gallery = await Promise.all(
+        gallery.map(async (item) => {
+          const data = {
+            id: item.dbRef,
+            title: item.title,
+            href: "/admin/artwork/" + item.collection + "/" + item.dbRef,
+            size: item.size,
+            category: "mnft-miami",
+            collection: item.collection,
+            imageUrl: await getFullImageURL(item.imageUrl),
+            isMinted: item.isMinted,
+            mintinPrice: item.mintinPrice,
+          };
+
+          return data;
+        })
+      );
+
+      isLoading.value = false;
+
+    }
+
+    onMounted(async () => {
+      if (connect.value == "yes") {
+        emit("on:login");
+      }
+      await store.dispatch("blockchain/initWallets");
+      await store.dispatch("blockchain/getBlockChain");
       if (addr.value) {
         const mintValues = await getWhiteList(instance.value, addr.value);
         price.value = parseFloat(mintValues.price);
         amount.value = parseFloat(mintValues.amount);
       }
       await getData();
-      window.addEventListener("scroll", handleScroll)
+      window.addEventListener("scroll", handleScroll);
     });
 
-    onUnmounted(async() => {
-       window.removeEventListener("scroll", handleScroll)
-       
+    onUnmounted(async () => {
+      window.removeEventListener("scroll", handleScroll);
     });
 
     return {
       // navigation,
       // features,
-     
+
       data,
       getMore,
       scrollComponent,
       artLink,
       isLoading,
       price,
-      amount
+      amount,
+      stats,
+      localFilters,
+      showNfts
       // footerNavigation,
-      
-    }
+    };
   },
-}
+};
 </script>
 <style scoped>
 .header-height {
